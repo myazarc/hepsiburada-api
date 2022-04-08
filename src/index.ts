f998ed80bdd7dd9bae6e27ebbf2c40a517fe78dc
myazarc <myazarc@yandex.com>
2022-04-09 02:08:50 +0300

    export mpopservice

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import ListingService from "./hepsiburada/Services/ListingService";
-const hepsiburada = { ListingService };
+import MPopService from "./hepsiburada/Services/MPopService";
+const hepsiburada = { ListingService, MPopService };
 export default hepsiburada;
-export { ListingService };
+export { ListingService, MPopService };
