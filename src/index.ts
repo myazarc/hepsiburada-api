deabd20a801a73c4d65c6cb6ada3b0efce5f923b
myazarc <myazarc@yandex.com>
2022-04-11 17:06:34 +0300

    exported OrderService and version upgrade

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import ListingService from "./hepsiburada/Services/ListingService";
 import MPopService from "./hepsiburada/Services/MPopService";
 import TestOrderService from "./hepsiburada/Services/TestOrderService";
-const hepsiburada = { ListingService, MPopService, TestOrderService };
+import OrderService from "./hepsiburada/Services/OrderService";
+const hepsiburada = { ListingService, MPopService, TestOrderService, OrderService };
 export default hepsiburada;
-export { ListingService, MPopService, TestOrderService };
+export { ListingService, MPopService, TestOrderService, OrderService };
